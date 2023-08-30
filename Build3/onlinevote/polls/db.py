81e1d29592e69c63e085959937e7901fb83d0a3b
Lalit Rawool <132126769+Ghost3306@users.noreply.github.com>
2023-08-31 01:31:18 +0530

    Add files via upload

---
+++ Build3/onlinevote/polls/db.py
@@ -0,0 +1,82 @@
+import mysql.connector
+import random
+from django.shortcuts import render
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="vote"
+    )    
+db =mydb.cursor()
+def select_all(tab_nm):
+    sql = "select * from "+tab_nm
+    db.execute(sql)
+    res = db.fetchall()
+    return res
+
+
+    
+
+def reg_admin(name,address,phone,age,org,password):
+    s = "select * from admins where phone = '"+phone+"'"
+    db.execute(s)
+    res = db.fetchall()
+    if len(res)!=0:
+        return False
+    
+    uniqueid = random.randint(11111,99999)         
+    sql="insert into admins(name,address,phone,age,organization,uniqueid,password) values(%s,%s,%s,%s,%s,%s,%s)"
+    val=(name,address,phone,age,org,uniqueid,password)
+    db.execute(sql,val)
+    mydb.commit()
+    return True
+    
+        
+    
+def reg_user(name,address,phone,age,votestatus,password):
+    s = "select * from users where phone = '"+phone+"'"
+    db.execute(s)
+    res = db.fetchall()
+    if len(res)!=0:
+        return False
+    
+    uniqueid = random.randint(11111,99999)         
+    sql="insert into admins(name,address,phone,age,organization,uniqueid,password) values(%s,%s,%s,%s,%s,%s,%s)"
+    val=(name,address,phone,age,votestatus,uniqueid,password)
+    db.execute(sql,val)
+    mydb.commit()
+    return True
+
+
+def show_tab():
+    db.execute('show tables')
+    res = db.fetchall()
+    return res
+
+
+def cr_election(str1,user):
+    sql = "create table if not exists "+str1+"(name varchar(30))"
+    sql1 = "insert into "+user+" values('"+str1+"')" 
+    db.execute(sql1)
+    mydb.commit()
+    db.execute(sql)
+    mydb.commit()
+    return True
+
+def insert_tab(tab_name,can_name):
+    sql = "INSERT INTO "+ tab_name+ "(name) VALUES ('"+can_name+"')"
+    db.execute(sql)
+    mydb.commit()
+    return True
+
+
+def verify(user,password):       
+    db.execute("select * from users")
+    res = db.fetchall()
+    for x in res:
+        if x[2]==user and x[6]==password:
+            return True
+        
+    return False
+
+print(reg_admin('lalit rawool','halwal','48474787883','20','college','root'))
---
+++ Build3/onlinevote/polls/views.py
@@ -0,0 +1,102 @@
+from django.shortcuts import *
+from .db import *
+from .file import *
+# Create your views here.
+def homepage(request):
+    return render(request,"homepage.html")
+
+
+def userl_login(request):
+
+
+    
+    return render(request,'user_login.html')
+
+
+def admin_login(request):
+    if request.method=='POST':
+        user_nm = request.POST.get('user')
+        password = request.POST.get('passw')
+        if verify(user_nm,password):
+            return HttpResponseRedirect('crelection')
+        else:
+            return render(request,'admin_login.html',{'msgtype':'error','message':'Invalid Credintials'})
+    return render(request,'admin_login.html')
+
+def register(request):
+    if request.method=='POST':
+        typeuser = request.POST.get('type')
+        crpass = request.POST.get('crpass')
+        cnpass = request.POST.get('cnpass')
+
+        name = request.POST.get('fname')+' '+request.POST.get('mname')+' '+request.POST.get('lname')
+        address_user = request.POST.get('address')
+        phone = request.POST.get('phone')
+        dob = request.POST.get('dob')
+        org = request.POST.get('org')
+        #have to calculate age before processing
+        if crpass ==cnpass:
+            if typeuser=='admin':
+                if reg_admin(name,address_user,phone,'20',org,crpass):
+                    return HttpResponseRedirect('adminlogin')
+                else:
+                    return render(request,'register.html',{'msgtype':'error','message':'Unexpected Error Occur'})
+            elif typeuser=='user':
+                pass
+            else:
+                return render(request,'register.html',{'msgtype':'error','message':'Please select User type!'})
+        else:
+            return render(request,'register.html',{'msgtype':'error','message':'Create and Confirm Password doesnt match'})
+        
+
+
+    return render(request,'register.html')
+can_nm = []
+def crelection(request):
+    context={
+        'gateway':'tab_cr'
+    }
+    if request.method=='POST':
+        electiob_nm = request.POST.get('elec_tab')
+        res = show_tab()
+        for tab in  res:
+            if electiob_nm == tab:
+                context = {
+                    'tab_cr':'error',
+                    'message':'ELection name cannot be set'
+
+                    }
+                return render(request,'crelection.html',context)
+        
+        if type(electiob_nm)==str:
+            
+            print('table create sucess table: ' ,electiob_nm)
+            write_tab(electiob_nm)
+            if cr_election(electiob_nm):
+                
+                context = {
+                    'title':'Admin Login',
+                    'gateway':'comp_tab',
+                    'table':electiob_nm
+                }
+                return render(request,'admin.html',context)
+        can_name = request.POST.get('can_nm')
+        if type(can_name)==str:
+            table_name = read_tab()
+            if insert_tab(table_name,can_name):
+
+                can_nm.append(can_name)
+                print('candidate name is :',can_name)
+                context = {
+                        'title':'Admin Login',
+                        'gateway':'comp_tab',
+                        'table':electiob_nm,
+                        'candidate' :can_nm
+                    }
+                return render(request,'admin.html',context)
+   
+
+    return render(request,'crelection.html',context)
+
+def search_election(request):
+    return render(request,'search_election.html')
