a730e2f6f70ee37e2bbf3c5bdd4f8978b2194e2f
Lalit Rawool <132126769+Ghost3306@users.noreply.github.com>
2023-09-08 20:52:51 +0530

    Add files via upload

---
+++ Build5/onlinevote/polls/db.py
@@ -0,0 +1,113 @@
+import mysql.connector
+import random
+from django.shortcuts import render
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="vote"
+    )    
+db =mydb.cursor()
+def select_all(tab_nm):
+    sql = "select * from "+tab_nm
+    db.execute(sql)
+    res = db.fetchall()
+    return res
+
+
+def create_admin_tab(name): 
+    sql = "create table if not exists "+name+"(name varchar(30))"
+    db.execute(sql)
+    mydb.commit()
+    return True
+
+
+
+def reg_admin(name,email,phone,age,address,typeuser,password):
+    s = "select * from allusers where phone = '"+phone+"'"
+    db.execute(s)
+    res = db.fetchall()
+    if len(res)!=0:
+        print('user already exist')
+        return False
+    
+        
+    sql="insert into allusers(name,email,phone,age,address,typeuser,password) values(%s,%s,%s,%s,%s,%s,%s)"
+    val=(name,email,phone,age,address,typeuser,password)
+    db.execute(sql,val)
+    mydb.commit()
+    
+    return True
+    
+        
+    
+def reg_user(name,email,phone,age,address,typeuser,password):
+    s = "select * from allusers where phone = '"+phone+"'"
+    db.execute(s)
+    res = db.fetchall()
+    if len(res)!=0:
+        print('user already exist')
+        return False
+    
+       
+    sql="insert into allusers(name,email,phone,age,address,typeuser,password) values(%s,%s,%s,%s,%s,%s,%s)"
+    val=(name,email,phone,age,address,typeuser,password)
+    db.execute(sql,val)
+    mydb.commit()
+    print('user registered successful')
+    return True
+
+
+def show_tab():
+    db.execute('show tables')
+    res = db.fetchall()
+    return res
+
+
+def cr_election(str1):
+    sql = "create table if not exists "+str1+"(name varchar(30))"
+    db.execute(sql)
+    mydb.commit()
+    return True
+
+def insert_tab(tab_name,can_name):
+    sql = "INSERT INTO "+ tab_name+ "(name) VALUES ('"+can_name+"')"
+    db.execute(sql)
+    mydb.commit()
+    return True
+
+
+def verify(user,password):       
+    db.execute("select * from allusers")
+    res = db.fetchall()
+    print('Given : ',user,password)
+    for x in res:
+        
+        print(x[1],x[6])
+        if x[1]==user and x[6]==password:
+            print('authenticate admin detected')
+            return x[5]
+        
+    return False
+
+def verify_user(user,password):       
+    db.execute("select * from users")
+    res = db.fetchall()
+    print('Given : ',user,password)
+    for x in res:
+        
+        print(x[3],x[7])
+        if x[3]==user and x[7]==password:
+            print('authenticate admin detected')
+            return True
+        
+    return False
+
+def tab_selection(tab_name):
+    s = "show tables"
+    db.execute(s)
+    res = db.fetchall()
+    for i in res:
+        if i==tab_name:
+            return False
+    
